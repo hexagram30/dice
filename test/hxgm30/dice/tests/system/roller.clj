51906aa53a79badaab56d08ef216b15f2f749038
Duncan McGreggor <oubiwann@gmail.com>
2019-12-30 21:32:27 -0600

    Added more tests.

--- test/hxgm30/dice/tests/system/roller.clj
+++ test/hxgm30/dice/tests/system/roller.clj
@@ -57,3 +57,19 @@
          (set (mapv (fn [_] (roller/d20 (system))) (range 1000)))))
   (is (= #{1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20}
          (apply set/union (mapv (fn [_] (remove-sum (roller/d20 (system) 100))) (range 1000))))))
+
+(deftest roll
+  (let [roll-count 10000]
+    (is (= [5] (map (comp int #(/ % roll-count) last) (roller/roll (system) {:d10 roll-count}))))
+    (let [rolls (into {} (map #(vector % roll-count) [:d4 :d6 :d8 :d10 :d12 :d20 :d100]))
+          expected (map #(* (/ % 2) roll-count) [4 6 8 10 12 20 100])
+          results (map (comp #(* % roll-count) #(Math/round %) float #(/ % roll-count) last)
+                       (roller/roll (system) rolls))
+          paired (partition 2 (interleave expected results))]
+      (is (= #{0 1}
+             (->> paired
+                  (map (comp int
+                             #(Math/abs %)
+                             float
+                             #(/ (- (first %) (second %)) roll-count)))
+                  (into #{})))))))
